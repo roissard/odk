2712e86bac3dc3a0bd381afea69d4e5dae555c29
CartONG staff <info@cartong.org>
2019-06-21 09:32:40 +0200

    Update install.sh
    missing library

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 curl -sSL https://get.docker.com | sh
 
-apt-get install -y python python-pip libffi-dev
+apt-get install -y python python-pip libffi-dev libssl-dev
 pip install docker-compose
 
 cp odk.service /etc/systemd/system/
